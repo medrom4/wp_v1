13e811e20deef606a8390a3e12c43dd983a662dc
Ramanovich <medikroma@gmail.com>
2018-12-04 16:19:55 +0000

    nav-bara

--- wp-content/themes/nifty-lite/sections/specia-navigation.php
+++ wp-content/themes/nifty-lite/sections/specia-navigation.php
@@ -5,7 +5,7 @@
 
 			<!-- Mobile Display -->
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<button type="button" class="navbar-toggle collapsed c-hamburger c-hamburger--htx" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 					<span class="sr-only"><?php _e('Toggle navigation','nifty-lite');?></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
